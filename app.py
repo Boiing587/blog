d0321fcbacb869afcb95e483b71dadf77618c58e
Thomas Espervik <thomas.espervik@me.com>
2019-11-12 10:51:51 +0000

    Requirements and api keys added

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 from flask import Flask, send_from_directory
+import os
+from dotenv import load_dotenv()
 
 app = Flask(__name__, static_url_path='/public')
-
+os.getenv('PORT', 1337)
 
 @app.route('/')
 def form():
     return send_from_directory('./public', 'index.html')
 
-@app.route('/bruh')
+@app.route('/about')
 def bruh():
     return send_from_directory('./public', 'bruh.html')
 
