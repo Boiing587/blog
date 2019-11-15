ac1a319c8129d75b5ca775b80583c74b8920b9df
Thomas Espervik <thomas.espervik@me.com>
2019-11-15 17:35:30 +0000

    Sidebar improvements

---
+++ public/sidebar.js
@@ -0,0 +1,13 @@
+document.write(`
+
+  <div id='toolbar'>
+    <a id='sideHeader'>Navigation</a>
+    <a href='/'>Home</a>
+    <a href='/about.html'>About me</a
+    <a href='/blog.html'>Blog</a>
+    <a id='sideHeader'>External links</a>
+    <a href='https://www.repl.it/@Boiing587'>My repl.it page</a>
+    <a href='https://www.google.com'>goloogoloo</a>
+  </div>
+
+`);
