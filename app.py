7eea8c82fdcb5b05388f8aac5d11836a5a469912
Thomas Espervik <thomas.espervik@me.com>
2019-11-18 09:56:48 +0000

    Fixed fonts and stuff

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python3
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template, request, jsonify, redirect
 from dotenv import load_dotenv
-load_dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 app = Flask(__name__, static_url_path='/public')
-port = os.getenv('PORT', 1337)
+port = os.getenv('PORT', 8080)
+apiKey = os.getenv('API_KEY', '')
 
-@app.route('/')
-def form():
+@app.route('/', methods=['GET'])
+def index():
     return send_from_directory('./public', 'index.html')
-
-@app.route('/about')
-def bruh():
-    return send_from_directory('./public', 'bruh.html')
 
 @app.route('/<path:path>', methods=['GET'])
 def serve(path):
-    return send_from_directory('public', path)
+    return send_from_directory('./public', path)
+
+@app.route('/compose', methods=['GET'])
+def compose():
+    if request.args['api_key'] == apiKey:
+        return render_template('compose.html', message=request.args['api_key'])
+    else:
+        return '<h1>Wait that\'s <strong>illegal!</strong></h1>'
+
+@app.route('/post', methods=['GET'])
+def post():
+    if request.args['api_key'] == apiKey:
+        if 'title' in request.args and 'content' in request.args:
+            print('Title: ' + request.args.get('title'))
+            print('Content: ' + request.args.get('content'))
+            return redirect('/compose?api_key=' + request.args['api_key'])
+    else:
+        return '<h1>Wait that\'s <strong>illegal!</strong></h1>'
+
+
+@app.route('/test')
+def test():
+    return send_from_directory('./public', 'test.html')
 
 
 app.run(host='0.0.0.0', port=port)
--- public/sidebar.js
+++ public/sidebar.js
@@ -3,7 +3,7 @@ document.write(`
   <div id='toolbar'>
     <a id='sideHeader'>Navigation</a>
     <a href='/'>Home</a>
-    <a href='/about.html'>About me</a
+    <a href='/about.html'>About me</a>
     <a href='/blog.html'>Blog</a>
     <a id='sideHeader'>External links</a>
     <a href='https://www.repl.it/@Boiing587'>My repl.it page</a>
