b3f9bb8f81ae09a1b31cd0c387840f80eba56b9e
Thomas Espervik <thomas.espervik@me.com>
2019-11-25 09:44:56 +0000

    it finally works

--- app.py
+++ app.py
@@ -28,31 +28,21 @@ def compose():
 def post():
     if request.args['api_key'] == apiKey:
         if 'title' in request.args and 'content' in request.args and 'author' in request.args:
-            """ Slik vil posts.json se ut
-            [
-                {
-                    "author": "Thomas",
-                    "title": "Blog 1",
-                    "content": "this is my epic blog"
-                },
-                {
-                    "author": "Thomas",
-                    "title": "Blog 2",
-                    "content": "this is my epicer blog"
-                }
-            ]
-            """
-            with open('public/posts.json', 'r') as knut:    # Open posts.json
-                content = json.loads(knut.read())           # Load current posts into variable
+            if not os.path.isfile('./public/posts.json'):
+                with open('public/posts.json', 'w+') as file:
+                    file.write("[]")
+            with open('public/posts.json', 'r') as file:    # Open posts.json in read mode
+                content = json.loads(file.read())           # Load current posts into variable
                 content.append({                            # Append a new post object
-                    "title": request.args.get("title"),
-                    "author": request.args.get("author"),
+                    "title": request.args.get("title"),     # Dette skriver ikke 'request.args.get("title") til content,
+                    "author": request.args.get("author"),   #  men outputen av det
                     "content": request.args.get("content")
                 })
-            with open('public/posts.json', 'w') as knut:
-                knut.write(str(content))
-            with open('public/posts.json', 'r') as knut:
-                return jsonify(json.loads(knut.read()))
+            with open('public/posts.json', 'w') as file:    # Open posts.json in write mode
+                file.write(str(json.dumps(content)))
+            return redirect('/blog.html')
+            #with open('public/posts.json', 'r') as file:
+            #    return jsonify(json.loads(file.read()))     # Return the contents of posts.json
     else:
         return '<h1>Wait that\'s <strong>illegal!</strong></h1>'
 
