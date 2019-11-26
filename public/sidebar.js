5e92a816a4b9e3a77f1bbf2d914d0ae5b2b3b67c
Thomas Espervik <thomas.espervik@me.com>
2019-11-26 15:06:11 +0100

    pls

--- public/sidebar.js
+++ public/sidebar.js
@@ -1,3 +1,43 @@
+document.write(`<link rel="stylesheet" href="${localStorage.getItem('mode')}.css" id='mode'`);
+mode = document.getElementById('mode');
+localStorage.setItem('mode', 'styles');
+
+function lightMode(){
+  mode.setAttribute('href', 'light.css');
+}
+
+function lightStored(){
+  localStorage.setItem('mode', 'light');
+}
+
+function darkMode(){
+  mode.setAttribute('href', 'styles.css');
+}
+
+function darkStored(){
+  localStorage.setItem('mode', 'styles');
+}
+
+function light(){
+  lightMode();
+  lightStored();
+}
+
+function dark(){
+  darkMode();
+  darkStored();
+}
+/*
+function getMode(){
+  var x = localStorage.getItem('mode')
+  if (x == "light") {
+    mode.setAttribute('href', 'light')
+  } else if (x == "dark") {
+    mode.setAttribute('href', 'styles')
+  }
+}
+*/
+
 document.write(`
 
   <div id='toolbar'>
@@ -16,42 +56,4 @@ document.write(`
 
 `);
 
-document.write(`<link rel="stylesheet" href="${localStorage.getItem('mode')}.css" id='mode'`)
-mode = document.getElementById('mode');
-localStorage.setItem('mode', 'styles')
 
-function lightMode(){
-  mode.setAttribute('href', 'light.css')
-}
-
-function lightStored(){
-  localStorage.setItem('mode', 'light');
-}
-
-function darkMode(){
-  mode.setAttribute('href', 'styles.css')
-}
-
-function darkStored(){
-  localStorage.setItem('mode', 'styles');
-}
-
-function light(){
-  lightMode()
-  lightStored()
-}
-
-function dark(){
-  darkMode()
-  darkStored()
-}
-/*
-function getMode(){
-  var x = localStorage.getItem('mode')
-  if (x == "light") {
-    mode.setAttribute('href', 'light')
-  } else if (x == "dark") {
-    mode.setAttribute('href', 'styles')
-  }
-}
-*/
