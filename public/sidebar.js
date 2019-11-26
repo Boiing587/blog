d724ece2a2e1a8fcd81dad7aab7a92ba9ab14668
Thomas Espervik <thomas.espervik@me.com>
2019-11-26 08:14:27 +0000

    gettin sum cookies

--- public/sidebar.js
+++ public/sidebar.js
@@ -8,6 +8,49 @@ document.write(`
     <a id='sideHeader'>External links</a>
     <a href='https://www.repl.it/@Boiing587'>My repl.it page</a>
     <a href='https://www.google.com'>goloogoloo</a>
+    <div class='buttons'>
+      <button class='button' onclick='light()'>🔅</button>
+      <button class='button' onclick='dark()'>🌙</button>
+    </div>
   </div>
 
 `);
+
+mode = document.getElementById('mode');
+function lightMode(){
+  mode.setAttribute('href', 'light.css')
+}
+
+function lightCookie(){
+  document.cookie = "mode=light; expires=Fri, 1 Jan 2021 12:00:00 UTC; path=/";
+}
+
+function darkMode(){
+  mode.setAttribute('href', 'styles.css')
+}
+
+function darkCookie(){
+  document.cookie = "mode=dark; expires=Fri, 1 Jan 2021 12:00:00 UTC; path=/";
+}
+
+
+
+
+function light(){
+  lightMode()
+  lightCookie()
+}
+
+function dark(){
+  darkMode()
+  darkCookie()
+}
+
+function getCookie(){
+  var x = document.cookie;
+  if (x.indexOf('mode=light') == true) {
+    mode.setAttribute('href', 'light.css')
+  } else if (x.indexOf('mode=dark') == true) {
+    mode.setAttribute('href', 'styles.css')
+  }
+}
