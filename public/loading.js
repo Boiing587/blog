be44e7fd4b592b1faf42f583f8681544007067fc
Thomas Espervik <thomas.espervik@me.com>
2019-11-28 08:47:46 +0100

    testins some loading screens

---
+++ public/loading.js
@@ -0,0 +1,9 @@
+document.write(`
+    <link rel='stylesheet' href='loading.css'>
+    <div class='box'>
+        <div class='b b1'></div>
+        <div class='b b2'></div>
+        <div class='b b3'></div>
+        <div class='b b4'></div>
+    </div>
+`);
