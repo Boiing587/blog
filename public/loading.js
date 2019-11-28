0c9aec2419162c837216939c86cfa0128fea38fb
Thomas Espervik <thomas.espervik@me.com>
2019-11-28 09:00:38 +0100

    nope

--- public/loading.js
+++
@@ -1,8 +0,0 @@
-document.write(`
-    <div class='box'>
-        <div class='b b1'></div>
-        <div class='b b2'></div>
-        <div class='b b3'></div>
-        <div class='b b4'></div>
-    </div>
-`);
