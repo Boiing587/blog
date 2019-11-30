79224da20891327cd960912f885d93c1b8369642
Thomas Espervik <thomas.espervik@me.com>
2019-11-30 01:46:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,21 +35,24 @@ def compose():
 def post():
     if request.args['api_key'] == apiKey:
         if 'title' in request.args and 'content' in request.args and 'author' in request.args:
-            if not os.path.isfile('./public/posts.json'):
-                with open('public/posts.json', 'w+') as file:
-                    file.write("[]")
-            with open('public/posts.json', 'r') as file:    # Open posts.json in read mode
-                content = json.loads(file.read())           # Load current posts into variable
-                content.append({                            # Append a new post object
-                    "title": request.args.get("title"),     # Dette skriver ikke 'request.args.get("title") til content,
-                    "author": request.args.get("author"),   #  men outputen av det
-                    "content": request.args.get("content")
-                })
-            with open('public/posts.json', 'w') as file:    # Open posts.json in write mode
-                file.write(str(json.dumps(content)))
-            return redirect('/blog.html')
-            #with open('public/posts.json', 'r') as file:
-            #    return jsonify(json.loads(file.read()))     # Return the contents of posts.json
+            if request.args.get('title') == "" and request.args.get('content') == "" and request.args.get('author') == "":
+                return redirect('/compose?api_key=' + apiKey)
+            else:
+                if not os.path.isfile('./public/posts.json'):
+                    with open('public/posts.json', 'w+') as file:
+                        file.write("[]")
+                with open('public/posts.json', 'r') as file:    # Open posts.json in read mode
+                    content = json.loads(file.read())           # Load current posts into variable
+                    content.append({                            # Append a new post object
+                        "title": request.args.get("title"),     # Dette skriver ikke 'request.args.get("title") til content,
+                        "author": request.args.get("author"),   #  men outputen av det
+                        "content": request.args.get("content")
+                    })
+                with open('public/posts.json', 'w') as file:    # Open posts.json in write mode
+                    file.write(str(json.dumps(content)))
+                return redirect('/blog.html')
+                #with open('public/posts.json', 'r') as file:
+                #    return jsonify(json.loads(file.read()))     # Return the contents of posts.json
     else:
         return '<h1>Wait that\'s <strong>illegal!</strong></h1>'
 
