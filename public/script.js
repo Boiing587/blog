d64e6ca8ab2e501c2c02a6341d54ef7d62b91bd0
Thomas Espervik <thomas.espervik@me.com>
2020-01-06 09:13:46 +0100

    if no local storage exists, it should default to dark mode to prevent css not loading

--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
-document.write(`<link rel="stylesheet" href="${localStorage.getItem('mode')}.css" id='mode'>`);
 var mode = document.getElementById('mode');
+if(mode != 'light' && mode != 'styles') {
+  localStorage.setItem('mode', 'styles')
+}
+document.write(`<link rel="stylesheet" href="${localStorage.getItem('mode')}.css" id='mode'>`);
 
 function lightMode(){
   var mode = document.getElementById('mode');
